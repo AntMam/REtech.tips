93a122819fca8b93ee305572861e6b5c30595a54
AntMam <anthony.mameli@gmail.com>
2021-06-06 13:09:02 -0400

    edited footer submission form for netlify

--- components/Footer.vue
+++ components/Footer.vue
@@ -7,8 +7,9 @@
         Join our newsletter, we"ll send you updates
       </div>
       <div class="mx-auto w-full md:w-1/2">
-      <form name="contact" action="/thank-you/english.html" method="POST" data-netlify="true">
+      <form name="contact" action="/thank-you" method="POST" data-netlify="true">
         <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 items-center">
+          <input type="hidden" name="form-name" value="contact" />
           <input type="email" name="email" placeholder="Enter your email address" class="rounded mc text-center border-2 border-gray-500">
           <input type="submit" name="subscribe" value="Subscribe" class="mc-btn rounded px-2 py-1 w-full">
         </div>
