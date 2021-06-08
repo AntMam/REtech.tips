c94c5ec55f3ceb6fb960ea712e105269f58e6f9c
AntMam <anthony.mameli@gmail.com>
2021-06-08 05:17:26 -0400

    added social sharing card

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -82,7 +82,32 @@ export default {
         hid: 'description',
         name: 'description',
         content: this.article.description
-      }
+      },
+      {
+          hid: 'og:title',
+          property: 'og:title',
+          content: this.article.title
+        },
+        {
+          hid: 'og:description',
+          property: 'og:description',
+          content: this.article.description
+        },
+        {
+          hid: 'og:image',
+          property: 'og:image',
+          content: this.article.img
+        },
+        {
+          hid: 'og:image:secure_url',
+          property: 'og:image:secure_url',
+          content: this.article.img
+        },
+        {
+          hid: 'og:image:alt',
+          property: 'og:image:alt',
+          content: this.article.title
+        }
     ]
   }
 }
